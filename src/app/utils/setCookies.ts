fec86c363d79ae023558c0fde041dcdf77875dbb
akashdnet <khondokerakashmi@gmail.com>
2025-10-07 21:40:43 +0600

    cookies maxage added

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -6,12 +6,27 @@ export interface AuthTokens {
   refreshToken?: string;
 }
 
+
+
+
+
+// res.cookie('YOUR_COOKIE_NAME', token, {
+//   httpOnly: true,
+//   secure: true,
+//   sameSite: 'none',
+//   maxAge: COOKIE_EXPIRE_TIME,
+// });
+
+
+
+
 export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
-  if (tokenInfo.accessToken) {
-    res.cookie("access_token", tokenInfo.accessToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
+    if (tokenInfo.accessToken) {
+        res.cookie("access_token", tokenInfo.accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+              maxAge:  7 * 24 * 60 * 60 * 1000,
     });
   }
 
