9d3c5fe6810301582956f540cf111604df4a74f0
akashdnet <khondokerakashmi@gmail.com>
2025-10-15 15:50:16 +0600

    auth middleeare code updated 3: new auth with async/awit and new code

--- src/app/middlewares/Auth.ts
+++ src/app/middlewares/Auth.ts
@@ -2,24 +2,14 @@ import { envList } from "../config/envList"
 import jwt from "jsonwebtoken"
 import JwtTokenGenerator from "../utils/JwtTokenGenerator"
 import { UserModel } from "../modules/user/user.model"
-import { Request } from "express"
 import { setAuthCookie } from "../utils/setCookies"
-
-
 
 const ACCESS_SECRET = envList.JWT_ACCESS_SECRET
 const REFRESH_SECRET = envList.JWT_REFRESH_SECRET
 
-
-
 export default async function AuthGuard(req: any, res: any, next: any) {
-  
-
-  // console.log(`test cookies: ` ,req.cookies)
-
   const accessToken = req.cookies?.access_token
   const refreshToken = req.cookies?.refresh_token
-
 
   if (!accessToken || !refreshToken) {
     res.clearCookie("access_token")
@@ -27,60 +17,48 @@ export default async function AuthGuard(req: any, res: any, next: any) {
     return res.status(401).json({ message: "No access token or refresh token found!" })
   }
 
-
-  jwt.verify(accessToken, ACCESS_SECRET, async (err: any, decoded: any) => {
-
-    if (err && err.name === "TokenExpiredError") { 
-
+  try {
+    
+    const decoded: any = jwt.verify(accessToken, ACCESS_SECRET)
+    req.user = decoded
+    return next()
+  } catch (err: any) {
+    
+    if (err.name === "TokenExpiredError") {
       try {
-
         const decodedRefreshToken: any = jwt.verify(refreshToken, REFRESH_SECRET)
-        const user:any = await UserModel.findOne((u:any) => u._id === decodedRefreshToken.id)
+
+
+        const user = await UserModel.findOne({ _id: decodedRefreshToken.id })
+
         if (!user) {
           res.clearCookie("access_token")
           res.clearCookie("refresh_token")
           return res.status(401).json({ message: "Invalid refresh token!" })
         }
 
-        const newAccessToken = JwtTokenGenerator.AccessToken(user)
-        const newRefreshToken = JwtTokenGenerator.RefreshToken(user)
 
 
+        const new_data = { id: user.id, email: user.email }
+        const newAccessToken = JwtTokenGenerator.AccessToken(new_data)
+        const newRefreshToken = JwtTokenGenerator.RefreshToken(new_data)
 
-        setAuthCookie(res, {accessToken: newAccessToken, refreshToken: newRefreshToken});
-
-
-
-
-        // res.cookie("access_token", newAccessToken, { httpOnly: true, secure: false, maxAge: 7 * 24 * 60 * 60 * 1000 })
-        // res.cookie("refresh_token", newRefreshToken, { httpOnly: true, secure: false, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        
+        setAuthCookie(res, { accessToken: newAccessToken, refreshToken: newRefreshToken })
 
         req.user = { id: user.id, email: user.email }
         return next()
-      } catch(err:any) {
-        console.log(`
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          Invalid or expired refresh token`,err)
+      } catch (refreshErr: any) {
+        console.error("Invalid or expired refresh token", refreshErr)
         res.clearCookie("access_token")
         res.clearCookie("refresh_token")
         return res.status(403).json({ message: "Invalid or expired refresh token" })
       }
-    } else if (err) {
-        res.clearCookie("access_token")
-        res.clearCookie("refresh_token")
-      return res.status(403).json({ message: "Invalid access token" })
-    } else {
-      req.user = decoded
-      return next()
     }
-  })
+
+    
+    res.clearCookie("access_token")
+    res.clearCookie("refresh_token")
+    return res.status(403).json({ message: "Invalid access token" })
+  }
 }
