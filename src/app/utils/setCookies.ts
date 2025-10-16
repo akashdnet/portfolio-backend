e9aab915fb9f86ffc066c8039e72a9807dc03d7b
akashdnet <khondokerakashmi@gmail.com>
2025-10-16 22:43:41 +0600

    auth isse 5

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -6,31 +6,22 @@ export interface AuthTokens {
   refreshToken?: string;
 }
 
-// res.cookie('YOUR_COOKIE_NAME', token, {
-//   httpOnly: true,
-//   secure: true,
-//   sameSite: 'none',
-//   maxAge: COOKIE_EXPIRE_TIME,
-// });
+
 
 export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
   if (tokenInfo.accessToken) {
     res.cookie("access_token", tokenInfo.accessToken, {
       httpOnly: true,
-      secure: true,
+      secure: envList.NODE_ENV === "production",
       sameSite: "none",
-      // maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: "/",
     });
   }
 
   if (tokenInfo.refreshToken) {
     res.cookie("refresh_token", tokenInfo.refreshToken, {
       httpOnly: true,
-      secure: true,
+      secure: envList.NODE_ENV === "production",
       sameSite: "none",
-      // maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: "/",
     });
   }
 };
